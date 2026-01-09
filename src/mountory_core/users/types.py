94f1438239d7bfc820f616830c64152423e3a215
saltie2193 <saltie@protonmail.com>
2026-01-09 15:53:12 +0100

    Change type of UserId to `UUID4`, add new Field types for pydantic models.

--- src/mountory_core/users/types.py
+++ src/mountory_core/users/types.py
@@ -1,3 +1,20 @@
-import uuid
+from typing import Annotated
 
-UserId = uuid.UUID
+from pydantic import UUID4, EmailStr, StringConstraints, Field
+
+PASSWORD_MIN_LENGTH = 10
+PASSWORD_MAX_LENGTH = 255
+
+UserId = UUID4
+
+EmailField = Annotated[EmailStr, StringConstraints(max_length=255)]
+OptionalEmailField = Annotated[EmailField | None, Field(default=None)]
+
+FullNameField = Annotated[str, StringConstraints(max_length=255)]
+OptionalFullNameField = Annotated[FullNameField | None, Field(default=None)]
+
+PasswordField = Annotated[
+    str,
+    StringConstraints(min_length=PASSWORD_MIN_LENGTH, max_length=PASSWORD_MAX_LENGTH),
+]
+OptionalPasswordField = Annotated[PasswordField | None, Field(default=None)]
