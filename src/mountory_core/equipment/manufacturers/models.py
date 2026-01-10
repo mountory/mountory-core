125f3af6de72faa9b734622c847ab74f66cea349
saltie2193 <saltie@protonmail.com>
2026-01-10 20:29:20 +0100

    Cleanup manufacturer models
    - use pydantic for non database modes
    - remove unused models
    - introduce reusable field definitions for pydantic models

--- src/mountory_core/equipment/manufacturers/models.py
+++ src/mountory_core/equipment/manufacturers/models.py
@@ -1,24 +1,43 @@
 import uuid
 from typing import Annotated
 
-from pydantic import StringConstraints
+from pydantic import BaseModel
 from sqlmodel import Field, SQLModel
 
 from mountory_core.equipment.manufacturers.types import (
     ManufacturerAccessRole,
     ManufacturerId,
+    ManufacturerShortNameField,
+    ManufacturerDescriptionField,
+    ManufacturerWebsiteField,
+    OptionalManufacturerNameField,
+    ManufacturerNameField,
 )
 from mountory_core.types import OptionalWebsiteField, OptionalStr
 from mountory_core.users.types import UserId
 
-ManufacturerNameField = Annotated[
-    str,
-    Field(description="Name of the manufacturer."),
-    StringConstraints(min_length=2, max_length=255),
-]
+
+class ManufacturerCreate(BaseModel):
+    name: ManufacturerNameField
+    short_name: ManufacturerShortNameField = None
+    description: ManufacturerDescriptionField = None
+    website: ManufacturerWebsiteField = None
+    hidden: bool | None = None
 
 
-class ManufacturerBase(SQLModel):
+class ManufacturerUpdate(BaseModel):
+    name: OptionalManufacturerNameField = None
+    short_name: ManufacturerShortNameField = None
+    description: ManufacturerDescriptionField = None
+    website: ManufacturerWebsiteField = None
+    hidden: bool | None = None
+
+
+class Manufacturer(SQLModel, table=True):
+    __tablename__ = "equipment_manufacturer"
+
+    name: str
+    id: ManufacturerId = Field(default_factory=uuid.uuid4, primary_key=True)
     short_name: Annotated[
         OptionalStr,
         Field(default=None, description="Short name of the manufacturer."),
@@ -27,55 +46,21 @@ class ManufacturerBase(SQLModel):
         OptionalStr, Field(default=None, description="Description of the manufacturer.")
     ] = None
     website: OptionalWebsiteField = None
-
-
-class ManufacturerAccessBase(SQLModel):
-    user_id: UserId
-
-
-class ManufacturerUserAccessCreate(ManufacturerAccessBase):
-    role: ManufacturerAccessRole = Field(
-        description="New access right to set. If not set or ``None``, the access right will be removed.",
-    )
-
-
-class ManufacturerCreate(ManufacturerBase):
-    name: ManufacturerNameField
-    hidden: bool | None = None
-    accesses: list[ManufacturerUserAccessCreate] | None = None
-
-
-class ManufacturerUserAccessUpdateOrDelete(ManufacturerAccessBase):
-    role: ManufacturerAccessRole | None = Field(
-        default=None,
-        description="New access right to set. If not set or ``None``, the access right will be removed.",
-    )
-
-
-class ManufacturerUserAccessUpdate(ManufacturerUserAccessUpdateOrDelete):
-    role: ManufacturerAccessRole = Field(
-        description="New access right to set.",
-    )
-
-
-class ManufacturerUpdate(ManufacturerBase):
-    name: ManufacturerNameField
-    hidden: bool | None = None
-    accesses: list[ManufacturerUserAccessUpdate] | None = None
-
-
-class Manufacturer(ManufacturerBase, table=True):
-    __tablename__ = "equipment_manufacturer"
-
-    name: str
-    id: ManufacturerId = Field(default_factory=uuid.uuid4, primary_key=True)
     hidden: bool = Field(default=True, nullable=False)
 
 
 class ManufacturerAccess(SQLModel, table=True):
     __tablename__ = "equipment_manufacturer_access"
-    manufacturer_id: ManufacturerId = Field(
-        primary_key=True, foreign_key="equipment_manufacturer.id", ondelete="CASCADE"
-    )
-    user_id: UserId = Field(primary_key=True, foreign_key="user.id", ondelete="CASCADE")
+
+    manufacturer_id: Annotated[
+        ManufacturerId,
+        Field(
+            primary_key=True,
+            foreign_key="equipment_manufacturer.id",
+            ondelete="CASCADE",
+        ),
+    ]
+    user_id: Annotated[
+        UserId, Field(primary_key=True, foreign_key="user.id", ondelete="CASCADE")
+    ]
     role: ManufacturerAccessRole = Field(default=ManufacturerAccessRole.SHARED)
