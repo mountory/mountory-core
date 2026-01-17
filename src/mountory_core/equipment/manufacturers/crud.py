8bba043f89f92d71d969a010a9b16c5a2a35b84a
saltie2193 <saltie@protonmail.com>
2026-01-17 14:38:17 +0100

    Update `create_manufacturer` and `update_manufacturer` to no longer take input as data parameters
    - Pass values as separate parameters
    - Deprecate usage of old versions
    - Update tests

--- src/mountory_core/equipment/manufacturers/crud.py
+++ src/mountory_core/equipment/manufacturers/crud.py
@@ -1,3 +1,7 @@
+from typing import overload
+from typing_extensions import deprecated, Literal
+
+from pydantic import HttpUrl
 from collections.abc import Collection, Sequence
 
 from sqlalchemy import BinaryExpression, ColumnElement, delete, func, update
@@ -20,11 +24,76 @@ from mountory_core.users.models import User
 from mountory_core.users.types import UserId
 
 
-async def create_manufacturer(
-    *, db: AsyncSession, data: ManufacturerCreate, commit: bool = True
+async def _create_manufacturer(
+    db: AsyncSession,
+    *,
+    name: str,
+    short_name: str | None = None,
+    description: str | None = None,
+    website: HttpUrl | None = None,
+    hidden: bool | None = None,
+    id_: ManufacturerId | None = None,
+    commit: bool = True,
 ) -> Manufacturer:
+    """Create a manufacturer in the given database.
+
+    :param db: Database session.
+    :param name: Name of the manufacturer.
+    :param short_name: Optional short name or abbreviation of the manufacturer.
+        An empty string is interpreted as ``None``. (Default: ``None``)
+    :param description: Optional description of the manufacturer.
+        An empty string is interpreted as ``None``.(Default: ``None``)
+    :param website: Optional website of the manufacturer.
+        An empty string is interpreted as ``None``. (Default: ``None``)
+    :param hidden: Whether the manufacturer is hidden or not. (Default: ``True``)
+    :param id_: Optional ID of the manufacturer. (Default: ``None``)
+    :param commit: Whether to commit the database transaction. (Default: ``True``)
+
+    :return: Created manufacturer instance.
     """
-    Create a manufacturer in the given database.
+    logger.info(f"create_manufacturer {name}")
+
+    if short_name == "":
+        short_name = None
+    if description == "":
+        description = None
+    if website == "":
+        website = None
+
+    logger.info(f"create_manufacturer {name}, create object")
+    manufacturer = Manufacturer(
+        name=name,
+        short_name=short_name,
+        description=description,
+        website=website,
+    )
+    # This is only in case the wrapping function passes ``None`` as the default.
+    # Can be removed if the old function is removed and the wrapper does not exist anymore.
+    if hidden is not None:
+        logger.debug(f"create_manufacturer {name}, handle hidden ({hidden=})")
+        manufacturer.hidden = hidden
+    if id_:
+        logger.debug(f"create_manufacturer {name}, handle id (id={id_})")
+        manufacturer.id = id_
+
+    logger.info(f"create_manufacturer {name}, add to database, {manufacturer=}")
+    db.add(manufacturer)
+    if commit:
+        logger.debug("create_manufacturer, commit transaction")
+        await db.commit()
+        await db.refresh(manufacturer)
+    return manufacturer
+
+
+@overload
+@deprecated("Pass values as separate parameters instead")
+async def create_manufacturer(
+    db: AsyncSession, *, data: ManufacturerCreate, commit: bool = True
+) -> Manufacturer:
+    """Create a manufacturer in the given database.
+
+    DEPRECATED: Pass values as separate parameters instead.
+        Passing values as single ``data`` parameter will be removed in a future release.
 
     :param db: Database session.
     :param data: ``ManufacturerCreate`` instance with data to create manufacturer.
@@ -32,19 +101,48 @@ async def create_manufacturer(
 
     :return: Created manufacturer.
     """
-    logger.info(f"create_manufacturer {data.name}")
-    model_data = data.model_dump(exclude_unset=True)
-    logger.debug(f"create_manufacturer, create object, {model_data=}")
-    manufacturer = Manufacturer.model_validate(model_data)
 
-    logger.debug(f"create_manufacturer, add to database, {manufacturer=}")
-    db.add(manufacturer)
-    if commit:
-        logger.debug("create_manufacturer, commit transaction")
-        await db.commit()
-        await db.refresh(manufacturer)
 
-    return manufacturer
+@overload
+async def create_manufacturer(
+    db: AsyncSession,
+    *,
+    name: str,
+    short_name: str | None = None,
+    description: str | None = None,
+    website: HttpUrl | None = None,
+    hidden: bool | None = None,
+    id_: ManufacturerId | None = None,
+    commit: bool = True,
+) -> Manufacturer: ...
+
+
+async def create_manufacturer(
+    db: AsyncSession,
+    *,
+    data: ManufacturerCreate | None = None,
+    name: str | None = None,
+    short_name: str | None = None,
+    description: str | None = None,
+    website: HttpUrl | None = None,
+    hidden: bool | None = None,
+    id_: ManufacturerId | None = None,
+    commit: bool = True,
+) -> Manufacturer:
+    if data is not None:
+        return await _create_manufacturer(db=db, **data.model_dump(), commit=commit)
+    if name is None:
+        raise ValueError("name is required")
+    return await _create_manufacturer(
+        db=db,
+        name=name,
+        short_name=short_name,
+        description=description,
+        website=website,
+        hidden=hidden,
+        id_=id_,
+        commit=commit,
+    )
 
 
 async def read_manufacturer_by_id(
@@ -177,36 +275,123 @@ async def read_manufacturers(
     return list((await db.exec(stmt)).all()), (await db.exec(count_stmt)).one()
 
 
-async def update_manufacturer_by_id(
-    *,
+async def _update_manufacturer_by_id(
     db: AsyncSession,
+    *,
     manufacturer_id: ManufacturerId,
-    data: ManufacturerUpdate,
+    name: str | None = None,
+    short_name: str | Literal[""] | None = None,
+    description: str | Literal[""] | None = None,
+    website: HttpUrl | str | Literal[""] | None = None,
+    hidden: bool | None = None,
     commit: bool = True,
 ) -> None:
-    """
-    Update a given manufacturer.
+    """Create a manufacturer in the given database.
 
     :param db: Database session.
     :param manufacturer_id: ``ManufacturerId`` of the manufacturer to update.
-    :param data: Data to update the manufacturer with.
+    :param name: Update name of the manufacturer.
+        If not provided will not be updated. (Default: ``None``)
+    :param short_name: Update short name or abbreviation of the manufacturer.
+        To remove, pass an empty string. (Default: ``None``)
+    :param description: Update description of the manufacturer.
+        To remove, pass an empty string. (Default: ``None``)
+    :param website: Optional website of the manufacturer.
+        To remove, pass an empty string. (Default: ``None``)
+    :param hidden: Whether the manufacturer is hidden or not. (Default: ``True``)
     :param commit: Whether to commit the database transaction. (Default: ``True``)
 
-    :return: ``None``
+    :raise: ``ValueError`` when an empty string is provided as ``name``.
+
+    :return: Created manufacturer instance.
     """
-    logger.info(
-        f"update_manufacturer_by_id, manufacturer_id={manufacturer_id}, data={data=}"
-    )
+    logger.info(f"update_manufacturer_by_id, manufacturer_id={manufacturer_id}")
+    if name == "":
+        raise ValueError("Name cannot be empty")
 
-    model_data = data.model_dump(exclude_unset=True)
-    logger.debug(f"update_manufacturer_by_id, data={model_data}")
+    data: dict[str, str | HttpUrl | bool | None] = {}
+    if short_name is not None:
+        data["short_name"] = None if short_name == "" else short_name
+    if description is not None:
+        data["description"] = None if description == "" else description
+    if website is not None:
+        data["website"] = None if website == "" else website
 
-    stmt = update(Manufacturer).filter_by(id=manufacturer_id).values(model_data)
+    if name is not None:
+        data["name"] = name
+    if hidden is not None:
+        data["hidden"] = hidden
+
+    if not data:
+        logger.info("update_manufacturer_by_id, no data provided")
+        return
+    logger.debug(f"update_manufacturer_by_id, data={data}")
+    stmt = update(Manufacturer).filter_by(id=manufacturer_id).values(data)
     await db.exec(stmt)
 
     if commit:
         logger.debug("update_manufacturer_by_id, commit transaction")
         await db.commit()
+
+
+@overload
+async def update_manufacturer_by_id(
+    db: AsyncSession,
+    *,
+    manufacturer_id: ManufacturerId,
+    data: ManufacturerUpdate,
+    commit: bool = True,
+) -> None: ...
+
+
+@overload
+async def update_manufacturer_by_id(
+    db: AsyncSession,
+    *,
+    manufacturer_id: ManufacturerId,
+    name: str | None = None,
+    short_name: str | Literal[""] | None = None,
+    description: str | Literal[""] | None = None,
+    website: HttpUrl | str | Literal[""] | None = None,
+    hidden: bool | None = None,
+    commit: bool = True,
+) -> None: ...
+
+
+async def update_manufacturer_by_id(
+    db: AsyncSession,
+    *,
+    manufacturer_id: ManufacturerId,
+    data: ManufacturerUpdate | None = None,
+    name: str | None = None,
+    short_name: str | Literal[""] | None = None,
+    description: str | Literal[""] | None = None,
+    website: str | HttpUrl | Literal[""] | None = None,
+    hidden: bool | None = None,
+    commit: bool = True,
+) -> None:
+    if data is not None:
+        if "name" in data.model_fields_set:
+            name = data.name
+        if "short_name" in data.model_fields_set:
+            short_name = "" if data.short_name is None else data.short_name
+        if "description" in data.model_fields_set:
+            description = "" if data.description is None else data.description
+        if "website" in data.model_fields_set:
+            website = "" if data.website is None else data.website
+        if "hidden" in data.model_fields_set:
+            hidden = data.hidden
+
+    return await _update_manufacturer_by_id(
+        db=db,
+        manufacturer_id=manufacturer_id,
+        name=name,
+        short_name=short_name,
+        description=description,
+        website=website,
+        hidden=hidden,
+        commit=commit,
+    )
 
 
 async def delete_manufacturer_by_id(
